5e1d6930a8a8790e47a72d4d66a92fce5fb65407
Anish Pradhan <anishpradha5523@gmail.com>
2024-03-18 15:11:04 +0530

    TickAPi

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const app = express()
 
 const cors = require("cors")
 
+app.use((req, res, next) => {
+
+    res.header("Access-Control-Allow-Origin", "*")
+    
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    
+    if (req.method == 'OPTIONS') {
+    
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        const requestedHeaders = req.header('Access-Control-Request-Headers')
+        
+        if (requestedHeaders) {
+        
+            res.header('Access-Control-Allow-Headers', requestedHeaders);
+        }
+        
+        
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(cors({
     origin:"*"
 }))
